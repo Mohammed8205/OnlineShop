c98c1b79e9f1d028cc34d6fe38dccb377cd2da41
jahir07 <jahir07@yahoo.com>
2019-11-25 20:29:23 -0500

    Added Order details

--- MyShop/MyShop.Core/Contracts/IBasketService.cs
+++ MyShop/MyShop.Core/Contracts/IBasketService.cs
@@ -14,6 +14,7 @@ namespace MyShop.Core.Contracts
         void RemoveFromBasket(HttpContextBase httpContext, string itemId);
         List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
         BasketSummaryViewModel GetBacketSummary(HttpContextBase httpContext);
+        void ClearBasket(HttpContextBase httpContext);
 
     }
 }
